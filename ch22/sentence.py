6f3e804ad49d015be00a983b346c0c8c2bbf38dd
teng <wwwsousoucom@126.com>
2017-08-29 21:54:20 +0800

    ch22

---
+++ ch22/sentence.py
@@ -0,0 +1,21 @@
+'''
+Sentence: access words by index
+'''
+import re
+import reprlib
+
+RE_WORD = re.compile('\w+')
+
+class Sentence:
+    def __init__(self, text):
+        self.text = text
+        self.words = RE_WORD.findall(text)
+
+    def __getitem__(self, item):
+        return self.words[item]
+
+    def __len__(self):
+        return len(self.words)
+
+    def __repr__(self):
+        return 'Sentence(%s)' % reprlib.repr(self.text)
